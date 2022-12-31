ca6029ba23c1cf6a2348a56711497160ec0f6ba5
P Deepanshu <97436890+deep60@users.noreply.github.com>
2022-12-31 08:39:27 +0530

    Add files via upload

---
+++ Astronauts.swift
@@ -0,0 +1,14 @@
+//
+//  Astronauts.swift
+//  Moonshot
+//
+//  Created by P Deepanshu on 03/11/22.
+//
+
+import Foundation
+
+struct Astronauts: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
